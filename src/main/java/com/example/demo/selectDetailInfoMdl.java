606796a5eeea5409d75c9f345962e184e7a1b008
groove-suzuki <suzuki@groove-system.jp>
2023-10-12 17:34:55 +0900

    レビュー依頼用

---
+++ src/main/java/com/example/demo/selectDetailInfoMdl.java
@@ -0,0 +1,30 @@
+package com.example.demo;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.util.LinkedCaseInsensitiveMap;
+
+public class selectDetailInfoMdl {
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+	
+	//詳細検索メソッドを呼び出し
+	public void selectDetailInfo(List detailInfo, String syainId) {
+		//DBに接続する
+		envFile e = new envFile();
+		jdbcTemplate = e.jdbcTemplate();
+		//社員マスタ、役職マスタ、住所マスタ、家族マスタ、入社日マスタで検索を行う
+		List selectResult = jdbcTemplate.queryForList(
+				"SELECT a.syainId,a.syainName,a.affiliation,a.baseSalary,b.position,b.position_Allowance,c.address,c.residentTax,d.familyStructure,d.family_Allowance, FLOOR(DATEDIFF(CURDATE(), e.hire_date) / 365) as serviceLength FROM "
+						+ "syainmst a LEFT JOIN positionmst b ON a.positionNum = b.positionNum "
+						+ "INNER JOIN addressmst c ON a.addressNum = c.addressNum "
+						+ "LEFT JOIN familymst d ON a.familyNum = d.familyNum "
+						+ "INNER JOIN hiredatemst e ON a.syainId = e.syainId WHERE a.syainId = ?",
+				syainId);
+		//詳細情報をLinkedCaseInsensitiveMapに代入し、detaiInfoで保持する
+		LinkedCaseInsensitiveMap infoRecord = (LinkedCaseInsensitiveMap) selectResult.get(0);
+		detailInfo.add(infoRecord);
+	}
+}
---
+++ src/main/java/com/resources/static/syainListScr.js
@@ -0,0 +1,89 @@
+$(function() {
+	
+	//全件検索ボタン押下時に呼び出す関数
+	$("#allSelectButton").on("click", function() {
+		//表示するテーブルを切り替える関数を呼び出し
+		toggleTableBody("#allInfoTable", "#filterInfoTable");
+		$("#selectErrorMsg").empty();
+	})
+	
+	//検索ボタン押下時に呼び出す関数
+	$("#selectButton").on("click", function() {
+		//入力値をチェックする
+		var inputName = $("#nameInput").val();
+		var regex = /^[ぁ-んァ-ン一-龯]{1,10}$/;
+		var checkResult = regex.test(inputName);
+
+		if (checkResult) {
+			//入力エラーメッセージを非表示にする
+			$("#inputErrorMsg").empty();
+			//入力値を送信、検索結果を取得する関数を呼び出し
+			filterSearch(inputName)
+		} else {
+			//入力エラーメッセージを表示する
+			var errorMsgHtml = "<div style='color: red;'>" + "社員名は10字以内の漢字、ひらがなで入力してください。" + "</div>";
+			$("#inputErrorMsg").html(errorMsgHtml);
+		}
+	})
+	//戻るボタン押下時に呼び出す関数
+	$("#backButton").on("click",function(){
+		//初期画面に戻る
+		window.history.back();
+	})
+})
+
+//入力値を送信、検索結果を取得する関数
+function filterSearch(inputName) {
+	//入力値をajaxで送信する
+	$.ajax({
+		url: "http://localhost:8080/filterSearch",
+		type: "POST",
+		contentType: "application/json; charset=UTF-8",
+		data: JSON.stringify({
+			inputValue: inputName
+		})
+	}).done(function(data) {
+		var errorMsg = data.errorMsg;
+		if (errorMsg.length == 0) {
+			//絞り込み検索結果、入力エラーメッセージをリセットにする
+			$("#filterInfoTable,#selectErrorMsg").empty();
+			var syainInfo = data.syainInfo;
+			//検索結果分、社員情報をテーブルに設定する
+			for (var i = 0; i < syainInfo.length; i++) {
+				var infoRecord = syainInfo[i];
+				$("#filterInfoTable").append(
+					"<tr>"
+					+ "<td id='syainId-" + i + "'>" + infoRecord.syainId + "</td>"
+					+ "<td><a href='' id='syainName-" + i + "' class='syainNameLink' onclick='handleClick(" + i + ")'>"
+					+ "<div>" + infoRecord.syainName + "</div></a></td>"
+					+ "<td>" + infoRecord.affiliation + "</td>"
+					+ "<td>" + (infoRecord.position || "") + "</td>"
+					+ "</tr>"
+				);
+			}
+			//テーブル内容の表示を切り替える関数を呼び出し
+			toggleTableBody("#filterInfoTable", "#allInfoTable");
+		} else {
+			//社員情報一覧表を非表示にする
+			$("#filterInfoTable,#allInfoTable").hide();
+			//検索エラーメッセージを表示する
+			var errorMsgHtml = "<div style='color: red;'>" + errorMsg + "</div>";
+			$("#selectErrorMsg").html(errorMsgHtml);
+		}
+	}).fail(function() {
+		console.log("failが実行された");
+	})
+}
+
+//社員名のリンクがクリックされた時に呼び出す関数
+function handleClick(index){
+	//クリックされた社員名に該当する社員IDを画面に設定し、保持する
+	$("#selectedSyainId").val($("#syainId-" + index).text());
+		//子画面を表示する
+		window.open('selectDetaileInfo', 'selectWindow', 'popup');
+}
+//テーブル内容の表示を切り替える関数
+function toggleTableBody(tbodyToShow, tbodyToHide) {
+	$(tbodyToShow).show();
+	$(tbodyToHide).hide();
+}
